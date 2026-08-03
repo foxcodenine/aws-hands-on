d665fee54ccdc239d4a47f7fd8d40a39f1e31aea
Chris Farrugia <foxcode9@gmail.com>
2026-08-03 17:57:33 +0100

    Add a Terraform validate job and gate the plan behind it

--- 05-multi_lambda_crud_with_api_gateway/terraform/providers.tf
+++ 05-multi_lambda_crud_with_api_gateway/terraform/providers.tf
@@ -9,16 +9,16 @@ terraform {
   }
 
   backend "s3" {
-    bucket       = "aws-hands-on-tfstate-725211237961"
-    key          = "05-multi_lambda_crud_with_api_gateway/terraform.tfstate"
-    region       = "eu-west-1"
+    bucket = "aws-hands-on-tfstate-725211237961"
+    key    = "05-multi_lambda_crud_with_api_gateway/terraform.tfstate"
+    region = "eu-west-1"
     # profile      = "developer"
     use_lockfile = true
   }
 }
 
 provider "aws" {
-  region  = "eu-west-1"
+  region = "eu-west-1"
   # profile = "developer"
 
   default_tags {
